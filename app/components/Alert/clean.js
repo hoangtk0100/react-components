83f3da26f869171b663a3603b61e502083b9e21f
HoangNguyen <hoangnv@nexlesoft.com>
2018-12-16 12:54:39 +0700

    FIx

--- app/components/Alert/clean.js
+++ app/components/Alert/clean.js
@@ -3,10 +3,20 @@ import { canUseDOM } from '../utils';
 /**
  * Remove Alert from DOM
  */
-export default () =>
-  canUseDOM &&
+export default () => {
+  if (!canUseDOM) {
+    return false;
+  }
+
   document.querySelectorAll('.rc-alert-anywhere').forEach(alertAnyWhereNode => {
     while (alertAnyWhereNode.firstChild) {
       alertAnyWhereNode.removeChild(alertAnyWhereNode.firstChild);
     }
   });
+
+  document
+    .querySelectorAll('.flag__rc-alert-anywhere')
+    .forEach(elm => document.body.removeChild(elm));
+
+  return true;
+};
